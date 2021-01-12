6e3395557612e4f6c409a503bf8a35f677456116
Mateusz <mateusz.harlos@gmail.com>
2021-01-12 15:37:12 +0000

    extension

--- polymorphism_lab_start/src/main/java/DebitCard.java
+++ polymorphism_lab_start/src/main/java/DebitCard.java
@@ -1,21 +1,13 @@
-public class DebitCard implements IScan{
-    private String cardNumber;
+public class DebitCard extends PaymentCard implements IScan{
     private int sortCode;
     private int accountNumber;
-    private String expiryDate;
-    private int securityNumber;
 
-    public DebitCard(String cardNumber, int sortCode, int accountNumber, String expiryDate, int securityNumber) {
-        this.cardNumber = cardNumber;
+    public DebitCard(String cardNumber, String expiryDate, int securityNumber, int sortCode, int accountNumber) {
+        super(cardNumber, expiryDate, securityNumber);
         this.sortCode = sortCode;
         this.accountNumber = accountNumber;
-        this.expiryDate = expiryDate;
-        this.securityNumber = securityNumber;
     }
 
-    public String getCardNumber() {
-        return this.cardNumber;
-    }
 
     public int getSortCode() {
         return this.sortCode;
@@ -25,13 +17,6 @@ public class DebitCard implements IScan{
         return this.accountNumber;
     }
 
-    public String getExpiryDate() {
-        return this.expiryDate;
-    }
-
-    public int getSecurityNumber() {
-        return this.securityNumber;
-    }
 
     public String scan() {
         return "Payment Complete";
