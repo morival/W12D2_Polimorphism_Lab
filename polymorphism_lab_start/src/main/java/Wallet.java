d8b590dabc5461f1ba16bd618985d4ec5565bdb9
Mateusz <mateusz.harlos@gmail.com>
2021-01-12 15:12:52 +0000

    mvp

---
+++ polymorphism_lab_start/src/main/java/Wallet.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Wallet {
+    private String name;
+    private ArrayList<IScan> items;
+
+
+    public Wallet(String name) {
+        this.name = name;
+        this.items = new ArrayList<IScan>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfItems() {
+        return this.items.size();
+    }
+
+    public void addItem(IScan item) {
+        this.items.add(item);
+    }
+
+}
